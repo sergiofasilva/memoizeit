ce272d37201cc87c8bf6843d573939a600558458
sergiofasilva <sergiogfasilva@gmail.com>
2023-04-18 19:00:48 +0100

    samples

---
+++ samples/promises.mjs
@@ -0,0 +1,28 @@
+import {memoizeit} from 'memoizeit';
+
+// function to be memoized
+async function promiseIncrementeOne(ms, value) {
+  return new Promise(resolve =>{
+    setTimeout(() => {
+      resolve(Number(value) + 1);
+    }, ms);
+  });
+}
+
+async function init(){
+  // memoization of the memoization of the promised function
+  const memoizedPromise = memoizeit(promiseIncrementeOne);
+
+  // using the memoized function
+  console.time("First time");
+  console.log('First call calculation:', await memoizedPromise(3000, 4)); // first call (no cached value)
+  console.timeEnd("First time");
+  console.log();
+  console.log(' #################### ');
+  console.log();
+  console.time("second time");
+  console.log('Second call calculation:', await memoizedPromise(3000, 4)); // second call (cache usage)
+  console.timeEnd("second time");
+}
+
+init();
