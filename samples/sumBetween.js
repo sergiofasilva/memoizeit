22d7e14a7ed0f98c93154f1c9153663e3a7146ed
salbuquerque <45566499+salbuquerque@users.noreply.github.com>
2023-04-20 17:31:54 +0100

    Deleted unneeded code.

--- samples/sumBetween.js
+++ samples/sumBetween.js
@@ -9,20 +9,16 @@ function getSumBetween(start, end) {
   return sum;
 }
 
-async function init() {
-  // memoization of the getSumBetween function
-  const memoizedGetSum = memoizeit(getSumBetween);
+// memoization of the getSumBetween function
+const memoizedGetSum = memoizeit(getSumBetween);
 
-  // using the memoized function
-  console.time('First time');
-  console.log('First calculation:', memoizedGetSum(1, 3000000000)); // first call (no cached value)
-  console.timeEnd('First time');
-  console.log();
-  console.log(' #################### ');
-  console.log();
-  console.time('Second time');
-  console.log('Second calculation:', memoizedGetSum(1, 3000000000)); // second call (cache usage)
-  console.timeEnd('Second time');
-}
-
-init();
+// using the memoized function
+console.time('First time');
+console.log('First calculation:', memoizedGetSum(1, 3000000000)); // first call (no cached value)
+console.timeEnd('First time');
+console.log();
+console.log(' #################### ');
+console.log();
+console.time('Second time');
+console.log('Second calculation:', memoizedGetSum(1, 3000000000)); // second call (cache usage)
+console.timeEnd('Second time');
