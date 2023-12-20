7d3e27fc917695057ffea241779e372f89ac59c7
Sérgio Silva <sergiofasilva@gmail.com>
2023-12-20 11:37:03 +0000

    -

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,8 +1,8 @@
-export = memoizeit;
-declare function memoizeit<Args extends unknown[], Out>(func: (...args: Args) => Out): (...args: Args) => Out;
 // export = memoizeit;
-// interface options {
-//     cache?: Map<any, any>
-//     ttl?: number
-// }
-// declare function memoizeit<Args extends unknown[], Out>(func: (...args: Args) => Out, options?: options): (...args: Args) => Out;
+// declare function memoizeit<Args extends unknown[], Out>(func: (...args: Args) => Out): (...args: Args) => Out;
+export = memoizeit;
+interface options {
+    cache?: Map<any, any>
+    ttl?: number
+}
+declare function memoizeit<Args extends unknown[], Out>(func: (...args: Args) => Out, options?: options): (...args: Args) => Out;
