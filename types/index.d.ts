605a289db34e47d1ae945c5071a1bef0e81d7ac8
Sérgio Silva <sergiofasilva@gmail.com>
2023-12-20 09:56:52 +0000

    test new ts declaration

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,2 +1,6 @@
 export = memoizeit;
-declare function memoizeit<Args extends unknown[], Out>(func: (...args: Args) => Out): (...args: Args) => Out;
+interface options {
+    cache?: Map<any, any>
+    ttl?: number
+}
+declare function memoizeit<Args extends unknown[], Out>(func: (...args: Args) => Out, options?: options): (...args: Args) => Out;
